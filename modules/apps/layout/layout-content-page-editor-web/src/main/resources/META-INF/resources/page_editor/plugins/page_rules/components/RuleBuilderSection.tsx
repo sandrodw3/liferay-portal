7d1bfa4fe4c378473726141add46bd988ca53a87
Victor Galan <victor.galan@liferay.com>
2023-10-31 12:19:47 +0100

    LPS-198741 Rename

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/page_rules/components/RuleBuilderSection.tsx
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/page_rules/components/RuleBuilderSection.tsx
@@ -14,8 +14,8 @@ import React, {useState} from 'react';
 import {v4 as uuidv4} from 'uuid';
 
 import {FragmentEntryLink} from '../../../app/actions/addFragmentEntryLinks';
-import Action, {Action as ActionType} from './Action';
-import Condition, {Condition as ConditionType} from './Condition';
+import ActionComponent, {Action} from './Action';
+import ConditionComponent, {Condition} from './Condition';
 
 const TriggerLabel = React.forwardRef<HTMLButtonElement, any>(
 	({children, className: _className, onClick, ...otherProps}, ref) => (
@@ -33,10 +33,10 @@ const TriggerLabel = React.forwardRef<HTMLButtonElement, any>(
 );
 
 type RuleBuilderActionProps = {
-	actions: ActionType[];
+	actions: Action[];
 	fragmentEntryLinks: FragmentEntryLink[];
 	layoutDataItems: {label: string; value: string}[];
-	setActions: (initializer: (previous: ActionType[]) => ActionType[]) => {};
+	setActions: (initializer: (previous: Action[]) => Action[]) => {};
 };
 
 export function RuleBuilderActionSection({
@@ -70,7 +70,7 @@ export function RuleBuilderActionSection({
 		>
 			<ClayPanel.Body>
 				{actions.map((action, index) => (
-					<Action
+					<ActionComponent
 						action={action}
 						key={action.id}
 						layoutDataItems={layoutDataItems}
@@ -100,7 +100,7 @@ export function RuleBuilderActionSection({
 					onClick={() =>
 						setActions((previousActions) => [
 							...previousActions,
-							{id: uuidv4()} as ActionType,
+							{id: uuidv4()} as Action,
 						])
 					}
 					size="sm"
@@ -113,10 +113,8 @@ export function RuleBuilderActionSection({
 }
 
 type RuleBuilderConditionProps = {
-	conditions: ConditionType[];
-	setConditions: (
-		initializer: (previous: ConditionType[]) => ConditionType[]
-	) => {};
+	conditions: Condition[];
+	setConditions: (initializer: (previous: Condition[]) => Condition[]) => {};
 };
 
 export function RuleBuilderConditionSection({
@@ -179,7 +177,7 @@ export function RuleBuilderConditionSection({
 		>
 			<ClayPanel.Body>
 				{conditions.map((condition, index) => (
-					<Condition
+					<ConditionComponent
 						condition={condition}
 						key={condition.id}
 						onConditionChange={(condition) =>
@@ -208,7 +206,7 @@ export function RuleBuilderConditionSection({
 					onClick={() =>
 						setConditions((previousConditions) => [
 							...previousConditions,
-							{id: uuidv4()} as ConditionType,
+							{id: uuidv4()} as Condition,
 						])
 					}
 					size="sm"
