2a25b4f243a248f844b87cc0bc16d44d04e7d192
mikellorza <mikel.lorza@liferay.com>
2024-01-04 16:01:28 +0100

    LPS-204982 Add friendly url separator configuration custom screen

---
+++ modules/apps/friendly-url/friendly-url-web/src/main/java/com/liferay/friendly/url/web/internal/portal/settings/configuration/admin/display/FriendlyURLSeparatorPortalSettingsConfigurationScreenWrapper.java
@@ -0,0 +1,92 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.friendly.url.web.internal.portal.settings.configuration.admin.display;
+
+import com.liferay.configuration.admin.display.ConfigurationScreen;
+import com.liferay.configuration.admin.display.ConfigurationScreenWrapper;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.settings.configuration.admin.display.PortalSettingsConfigurationScreenContributor;
+import com.liferay.portal.settings.configuration.admin.display.PortalSettingsConfigurationScreenFactory;
+
+import java.util.Locale;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Mikel Lorza
+ */
+@Component(service = ConfigurationScreen.class)
+public class FriendlyURLSeparatorPortalSettingsConfigurationScreenWrapper
+	extends ConfigurationScreenWrapper {
+
+	@Override
+	protected ConfigurationScreen getConfigurationScreen() {
+		return _portalSettingsConfigurationScreenFactory.create(
+			new FriendlyURLSeparatorPortalSettingsConfigurationScreenContributor());
+	}
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private PortalSettingsConfigurationScreenFactory
+		_portalSettingsConfigurationScreenFactory;
+
+	@Reference(target = "(osgi.web.symbolicname=com.liferay.friendly.url.web)")
+	private ServletContext _servletContext;
+
+	private class
+		FriendlyURLSeparatorPortalSettingsConfigurationScreenContributor
+			implements PortalSettingsConfigurationScreenContributor {
+
+		@Override
+		public String getCategoryKey() {
+			return "seo";
+		}
+
+		@Override
+		public String getJspPath() {
+			return "/configuration" +
+				"/friendly_url_separator_company_configuration.jsp";
+		}
+
+		@Override
+		public String getKey() {
+			return "friendly-url-separator-company-configuration";
+		}
+
+		@Override
+		public String getName(Locale locale) {
+			return _language.get(locale, "friendly-url");
+		}
+
+		@Override
+		public String getSaveMVCActionCommandName() {
+			return "/instance_settings" +
+				"/friendly_url_separator_save_company_configuration";
+		}
+
+		@Override
+		public ServletContext getServletContext() {
+			return _servletContext;
+		}
+
+		@Override
+		public boolean isVisible() {
+			if (!FeatureFlagManagerUtil.isEnabled("LPS-203351")) {
+				return false;
+			}
+
+			return true;
+		}
+
+	}
+
+}
