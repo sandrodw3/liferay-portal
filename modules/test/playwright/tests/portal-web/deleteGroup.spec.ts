489c2ba6c87bdc95f08e474386fc1914f5cc417e
Yang Cao <yang.cao@liferay.com>
2024-01-08 18:10:39 +0800

    Add a demo test

---
+++ modules/test/playwright/tests/portal-web/deleteGroup.spec.ts
@@ -0,0 +1,16 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {mergeTests} from '@playwright/test';
+
+import {serviceHelpersTest} from '../../fixtures/serviceHelpers.fixture';
+
+export const test = mergeTests(serviceHelpersTest);
+
+test('delete a group', async ({
+	_serviceHelpers
+}) => {
+	await _serviceHelpers.groupAdmin.deleteGroup("33081");
+});
