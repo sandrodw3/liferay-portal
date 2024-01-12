6796c981812edf10caa0eaea5f1bfe70432d356d
mikellorza <mikel.lorza@liferay.com>
2024-01-12 12:35:11 +0100

    LPS-205243 Validate if the separator is a number, since it may conflict with a layout friendlyURL because in last instance we are setting the layoutId as the friendlyURL of the layout.

--- modules/apps/friendly-url/friendly-url-web/src/main/java/com/liferay/friendly/url/web/internal/portlet/action/FriendlyURLSeparatorSaveCompanyConfigurationMVCActionCommand.java
+++ modules/apps/friendly-url/friendly-url-web/src/main/java/com/liferay/friendly/url/web/internal/portlet/action/FriendlyURLSeparatorSaveCompanyConfigurationMVCActionCommand.java
@@ -251,6 +251,16 @@ public class FriendlyURLSeparatorSaveCompanyConfigurationMVCActionCommand
 			return;
 		}
 
+		if (Validator.isNumber(
+				urlSeparator.substring(1, urlSeparator.length() - 1))) {
+
+			fieldsValidationErrorsJSONObject.put(
+				namespace + key,
+				_language.get(
+					themeDisplay.getLocale(),
+					"friendly-url-separator-error-can-not-be-a-number"));
+		}
+
 		String friendlyURL = urlSeparator.substring(
 			0, urlSeparator.length() - 1);
 
