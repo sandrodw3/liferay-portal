7ff5dfbf8951a0559f77ffa62398e759e44889b1
mikellorza <mikel.lorza@liferay.com>
2024-01-15 13:38:32 +0100

    LPS-205243 Validate that the separatorUrl does not conflict with: 1- /-/ especial friendly url separator 2-Other friendly url separator 3-Friendly url mappers 4-Language urls

--- modules/apps/friendly-url/friendly-url-web/src/main/java/com/liferay/friendly/url/web/internal/portlet/action/FriendlyURLSeparatorSaveCompanyConfigurationMVCActionCommand.java
+++ modules/apps/friendly-url/friendly-url-web/src/main/java/com/liferay/friendly/url/web/internal/portlet/action/FriendlyURLSeparatorSaveCompanyConfigurationMVCActionCommand.java
@@ -8,6 +8,7 @@ package com.liferay.friendly.url.web.internal.portlet.action;
 import com.liferay.configuration.admin.constants.ConfigurationAdminPortletKeys;
 import com.liferay.friendly.url.configuration.manager.FriendlyURLSeparatorConfigurationManager;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.LayoutFriendlyURLException;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
@@ -25,6 +26,9 @@ import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.service.impl.LayoutLocalServiceHelper;
+
+import java.util.Objects;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -226,6 +230,30 @@ public class FriendlyURLSeparatorSaveCompanyConfigurationMVCActionCommand
 				_language.get(
 					themeDisplay.getLocale(),
 					"friendly-url-separator-error-too-long"));
+
+			return;
+		}
+
+		try {
+			_layoutLocalServiceHelper.validateFriendlyURLKeyword(
+				urlSeparator.substring(0, urlSeparator.length() - 1));
+		}
+		catch (LayoutFriendlyURLException layoutFriendlyURLException) {
+			FriendlyURLResolver friendlyURLResolver =
+				FriendlyURLResolverRegistryUtil.getFriendlyURLResolver(
+					layoutFriendlyURLException.getKeywordConflict() +
+						StringPool.SLASH);
+
+			if ((friendlyURLResolver == null) ||
+				!Objects.equals(friendlyURLResolver.getKey(), key)) {
+
+				fieldsValidationErrorsJSONObject.put(
+					namespace + key,
+					_language.get(
+						themeDisplay.getLocale(),
+						"friendly-url-separator-error-other-asset-type-may-" +
+							"use-this-prefix"));
+			}
 		}
 	}
 
@@ -241,6 +269,9 @@ public class FriendlyURLSeparatorSaveCompanyConfigurationMVCActionCommand
 
 	@Reference
 	private Language _language;
+
+	@Reference
+	private LayoutLocalServiceHelper _layoutLocalServiceHelper;
 
 	@Reference
 	private Portal _portal;
