7ca67bb6ecf19c9e23bbd18c37668e804d7b7bfa
Jürgen Kappler <jurgen.kappler@liferay.com>
2024-02-20 15:10:19 +0100

    LPD-17782 Adds test

--- modules/test/playwright/tests/journal-web/fixtures/journalPagesTest.ts
+++ modules/test/playwright/tests/journal-web/fixtures/journalPagesTest.ts
@@ -5,13 +5,18 @@
 
 import {test} from '@playwright/test';
 
+import {JournalEditArticlePage} from '../pages/JournalEditArticlePage';
 import {JournalEditTemplatePage} from '../pages/JournalEditTemplatePage';
 import {JournalPage} from '../pages/JournalPage';
 
 const journalPagesTest = test.extend<{
+	journalEditArticlePage: JournalEditArticlePage;
 	journalEditTemplatePage: JournalEditTemplatePage;
 	journalPage: JournalPage;
 }>({
+	journalEditArticlePage: async ({page}, use) => {
+		await use(new JournalEditArticlePage(page));
+	},
 	journalEditTemplatePage: async ({page}, use) => {
 		await use(new JournalEditTemplatePage(page));
 	},
---
+++ modules/test/playwright/tests/journal-web/journalArticle.spec.ts
@@ -0,0 +1,74 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {expect, mergeTests} from '@playwright/test';
+
+import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
+import {applicationsMenuPageTest} from '../../fixtures/applicationsMenuPageTest';
+import {featureFlagsTest} from '../../fixtures/featureFlagsTest';
+import {loginTest} from '../../fixtures/loginTest';
+import getRandomString from '../../utils/getRandomString';
+import {journalPagesTest} from './fixtures/journalPagesTest';
+
+export const test = mergeTests(
+	apiHelpersTest,
+	applicationsMenuPageTest,
+	featureFlagsTest({
+		'LPD-16469': true,
+	}),
+	loginTest(),
+	journalPagesTest
+);
+
+const PERMISSIONS_LOCATORS = [
+	'#guest_ACTION_DELETE',
+	'#guest_ACTION_PERMISSIONS',
+];
+
+test('LPD-17782: This is a test for bulk permissions of web content', async ({
+	journalEditArticlePage,
+	journalPage,
+	page,
+}) => {
+	const title1 = getRandomString();
+	const title2 = getRandomString();
+
+	await journalEditArticlePage.goToCreateNewBasicArticle(title1);
+
+	const article1 = await page
+		.locator(
+			'#_com_liferay_journal_web_portlet_JournalPortlet_articlesSearchContainer .list-group-item'
+		)
+		.filter({hasText: title1});
+
+	await expect(article1).toBeVisible();
+
+	await journalEditArticlePage.goToCreateNewBasicArticle(title2);
+
+	const article2 = await page
+		.locator(
+			'#_com_liferay_journal_web_portlet_JournalPortlet_articlesSearchContainer .list-group-item'
+		)
+		.filter({hasText: title2});
+
+	await expect(article2).toBeVisible();
+
+	await journalPage.setJournalArticlePermissions(
+		[article1, article2],
+		PERMISSIONS_LOCATORS
+	);
+
+	await journalPage.assertJournalArticlePermissions(
+		title1,
+		PERMISSIONS_LOCATORS
+	);
+	await journalPage.assertJournalArticlePermissions(
+		title2,
+		PERMISSIONS_LOCATORS
+	);
+
+	await journalPage.deleteJournalArticle(title1);
+	await journalPage.deleteJournalArticle(title2);
+});
