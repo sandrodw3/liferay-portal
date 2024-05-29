0a4748dd2a17e148af9704f870671936a547028f
Jürgen Kappler <jurgen.kappler@liferay.com>
2024-05-29 10:26:38 +0200

    LPD-26298 Adds new enum for filter date types

--- modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/constants/ContentDashboardConstants.java
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/constants/ContentDashboardConstants.java
@@ -5,10 +5,56 @@
 
 package com.liferay.content.dashboard.web.internal.constants;
 
+import com.liferay.portal.kernel.search.Field;
+
+import java.util.Objects;
+
 /**
  * @author Stefan Tanasie
  */
 public class ContentDashboardConstants {
+
+	public enum DateType {
+
+		CREATE_DATE("create-date", Field.CREATE_DATE),
+		DISPLAY_DATE("display-date", Field.DISPLAY_DATE),
+		EXPIRATION_DATE("expiration-date", Field.EXPIRATION_DATE),
+		MODIFIED_DATE("modified-date", Field.MODIFIED_DATE),
+		PUBLISHED_DATE("published-date", Field.PUBLISH_DATE),
+		REVIEW_DATE("review-date", "reviewDate");
+
+		public static DateType parse(String type) {
+			for (DateType dateType : values()) {
+				if (Objects.equals(dateType.getType(), type)) {
+					return dateType;
+				}
+			}
+
+			return null;
+		}
+
+		public String getField() {
+			return _field;
+		}
+
+		public String getType() {
+			return _type;
+		}
+
+		@Override
+		public String toString() {
+			return _type;
+		}
+
+		private DateType(String type, String field) {
+			_type = type;
+			_field = field;
+		}
+
+		private final String _field;
+		private final String _type;
+
+	}
 
 	public enum DefaultInternalAssetVocabularyName {
 
