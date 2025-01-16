db5d2f5aa2f070287711de6c2f24760ce931a43d
Sandro Chinea <sandrochinea@gmail.com>
2025-01-16 11:24:58 +0000

    LPD-46011 Migrate to typescript

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/thunks/updateFragmentConfiguration.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/thunks/updateFragmentConfiguration.ts
@@ -3,6 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {State} from '../../types/State';
+import {updateNetwork} from '../actions';
+import {FragmentEntryLink} from '../actions/addFragmentEntryLinks';
 import updateFragmentEntryLinkConfiguration from '../actions/updateFragmentEntryLinkConfiguration';
 import {FREEMARKER_FRAGMENT_ENTRY_PROCESSOR} from '../config/constants/freemarkerFragmentEntryProcessor';
 import FragmentService from '../services/FragmentService';
@@ -11,6 +14,9 @@ import {clearPageContents} from '../utils/usePageContents';
 export default function updateFragmentConfiguration({
 	configurationValues,
 	fragmentEntryLink,
+}: {
+	configurationValues: Record<string, number | string>;
+	fragmentEntryLink: FragmentEntryLink;
 }) {
 	const {editableValues, fragmentEntryLinkId} = fragmentEntryLink;
 
@@ -19,11 +25,20 @@ export default function updateFragmentConfiguration({
 		[FREEMARKER_FRAGMENT_ENTRY_PROCESSOR]: configurationValues,
 	};
 
-	return (dispatch, getState) => {
+	return (
+		dispatch: (
+			action: ReturnType<
+				| typeof updateNetwork
+				| typeof updateFragmentEntryLinkConfiguration
+			>
+		) => void,
+		getState: () => State
+	) => {
 		const {languageId, segmentsExperienceId} = getState();
 
 		return FragmentService.updateConfigurationValues({
-			editableValues: nextEditableValues,
+			editableValues:
+				nextEditableValues as FragmentEntryLink['editableValues'],
 			fragmentEntryLinkId,
 			languageId,
 			onNetworkStatus: dispatch,
