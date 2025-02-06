0739806f86f26b8072b9ece27080c74472984625
Sandro Chinea <sandrochinea@gmail.com>
2025-02-06 22:19:08 +0000

    LPD-48502 Add and render StructureBuilder component

---
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/components/StructureBuilder.tsx
@@ -0,0 +1,14 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import React from 'react';
+
+import '../../../css/structure_builder/StructureBuilder.scss';
+
+export default function StructureBuilder() {
+	return (
+		<div className="d-flex flex-column structure-builder__wrapper"></div>
+	);
+}
