73501c77d057dce53962aaa6920adb155e544b15
Sandro Chinea <sandrochinea@gmail.com>
2025-02-10 12:31:04 +0000

    LPD-XXX Render fields and settings

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/components/StructureBuilder.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/components/StructureBuilder.tsx
@@ -6,13 +6,24 @@
 import React from 'react';
 
 import ManagementBar from './ManagementBar';
+import StructureFields from './StructureFields';
+import StructureSettings from './StructureSettings';
 
 import '../../../css/structure_builder/StructureBuilder.scss';
+import {StructureSettingsContextProvider} from '../contexts/StructureSettingsContext';
 
 export default function StructureBuilder() {
 	return (
 		<div className="d-flex flex-column structure-builder__wrapper">
-			<ManagementBar />
+			<StructureSettingsContextProvider>
+				<ManagementBar />
+
+				<div className="d-flex flex-grow-1 p-4">
+					<StructureFields />
+
+					<StructureSettings />
+				</div>
+			</StructureSettingsContextProvider>
 		</div>
 	);
 }
