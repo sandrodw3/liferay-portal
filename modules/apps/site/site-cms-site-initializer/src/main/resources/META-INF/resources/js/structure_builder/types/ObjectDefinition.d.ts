fbd5b2d15fd5abbd2cd1f0ed2b447b99b40e00f6
Sandro Chinea <sandrochinea@gmail.com>
2025-02-18 18:54:31 +0000

    LPD-48502 Add ObjectDefinition type

---
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/types/ObjectDefinition.d.ts
@@ -0,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {FieldBusinessType} from '../utils/fieldType';
+
+export type ObjectField = {
+	businessType: FieldBusinessType;
+	externalReferenceCode?: number;
+	label: {
+		en_US: string;
+	};
+	localized: boolean;
+	name: string;
+	required: boolean;
+};
+
+export type ObjectDefinition = {
+	id?: number;
+	label: {
+		en_US: string;
+	};
+	name: string;
+	objectFields?: ObjectField[];
+	pluralLabel: {
+		en_US: string;
+	};
+	scope: 'company';
+};
