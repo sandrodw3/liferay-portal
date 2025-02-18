360fc6cdec398745c5390c9d2b155a5dabfcc6e6
Sandro Chinea <sandrochinea@gmail.com>
2025-02-18 11:48:20 +0000

    LPD-48502 Add StateContext with useReducer

---
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/contexts/StateContext.tsx
@@ -0,0 +1,182 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import React, {
+	Dispatch,
+	ReactNode,
+	createContext,
+	useContext,
+	useReducer,
+} from 'react';
+
+import {ObjectField} from '../types/ObjectDefinition';
+import findAvailableFieldName from '../utils/findAvailableFieldName';
+import updateFields from '../utils/updateFields';
+
+const DEFAULT_STRUCTURE_NAME = Liferay.Language.get('untitled-structure');
+
+export type FieldType = 'text';
+
+type Status = 'new' | 'draft' | 'published';
+
+export type Field = {
+	erc?: number;
+	label: string;
+	name: string;
+	type: FieldType;
+};
+
+export type State = {
+	error: string | null;
+	fields: Map<string, Field>;
+	id: number | null;
+	name: string;
+	status: Status;
+};
+
+const INITIAL_STATE: State = {
+	error: null,
+	fields: new Map(),
+	id: null,
+	name: DEFAULT_STRUCTURE_NAME,
+	status: 'new',
+};
+
+type AddFieldAction = {field: Field; type: 'add-field'};
+
+type CreateStructureAction = {
+	id: number;
+	objectFields: ObjectField[];
+	type: 'create-structure';
+};
+
+type PublishStructureAction = {type: 'publish-structure'};
+
+type SetErrorAction = {error: string | null; type: 'set-error'};
+
+type SetNameAction = {name: string; type: 'set-name'};
+
+type UpdateStructureAction = {
+	objectFields: ObjectField[];
+	type: 'update-structure';
+};
+
+type Action =
+	| AddFieldAction
+	| CreateStructureAction
+	| PublishStructureAction
+	| UpdateStructureAction
+	| SetErrorAction
+	| SetNameAction;
+
+function reducer(state: State, action: Action) {
+	switch (action.type) {
+		case 'add-field': {
+			const {field} = action;
+
+			const name = findAvailableFieldName(state.fields, field.name);
+
+			const nextFields = new Map(state.fields);
+
+			nextFields.set(name, {...field, name});
+
+			return {...state, fields: nextFields};
+		}
+		case 'create-structure': {
+			const fields = updateFields(state.fields, action.objectFields);
+
+			return {
+				...state,
+				error: null,
+				fields,
+				id: action.id,
+				status: 'draft' as Status,
+			};
+		}
+		case 'publish-structure':
+			return {...state, error: null, status: 'published' as Status};
+		case 'update-structure': {
+			const fields = updateFields(state.fields, action.objectFields);
+
+			return {
+				...state,
+				error: null,
+				fields,
+			};
+		}
+		case 'set-error':
+			return {...state, error: action.error};
+		case 'set-name':
+			return {...state, name: action.name};
+		default:
+			return state;
+	}
+}
+
+const StateContext = createContext<{dispatch: Dispatch<Action>; state: State}>({
+	dispatch: () => {},
+	state: INITIAL_STATE,
+});
+
+export default function StateContextProvider({
+	children,
+}: {
+	children: ReactNode;
+}) {
+	const [state, dispatch] = useReducer<React.Reducer<State, Action>>(
+		reducer,
+		INITIAL_STATE
+	);
+
+	return (
+		<StateContext.Provider value={{dispatch, state}}>
+			{children}
+		</StateContext.Provider>
+	);
+}
+
+function useStateDispatch() {
+	return useContext(StateContext).dispatch;
+}
+
+function useStructureError() {
+	const {state} = useContext(StateContext);
+
+	return state.error;
+}
+
+function useStructureFields() {
+	const {state} = useContext(StateContext);
+
+	return Array.from(state.fields.values());
+}
+
+function useStructureId() {
+	const {state} = useContext(StateContext);
+
+	return state.id;
+}
+
+function useStructureName() {
+	const {state} = useContext(StateContext);
+
+	return state.name;
+}
+
+function useStructureStatus() {
+	const {state} = useContext(StateContext);
+
+	return state.status;
+}
+
+export {
+	StateContextProvider,
+	useStateDispatch,
+	useStructureError,
+	useStructureFields,
+	useStructureId,
+	useStructureName,
+	useStructureStatus,
+};
