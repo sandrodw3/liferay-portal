c6c6b647a90f050a32a95674f7c70e74ccd902fa
Sandro Chinea <sandrochinea@gmail.com>
2025-02-19 11:15:57 +0000

    LPD-48502 Separate logic to build object definition

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/services/StructureService.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/services/StructureService.ts
@@ -3,21 +3,12 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {API, objectDefinitionUtils} from '@liferay/object-js-components-web';
+import {API} from '@liferay/object-js-components-web';
 
-import {ObjectDefinition} from '../types/ObjectDefinition';
+import buildObjectDefinition from '../utils/buildObjectDefinition';
 
 async function saveStructure({name}: {name: string}) {
-	const objectDefinition: ObjectDefinition = {
-		label: {
-			en_US: name,
-		},
-		name: objectDefinitionUtils.normalizeName(name),
-		pluralLabel: {
-			en_US: name,
-		},
-		scope: 'company',
-	};
+	const objectDefinition = buildObjectDefinition({name});
 
 	await API.postObjectDefinition(objectDefinition);
 }
