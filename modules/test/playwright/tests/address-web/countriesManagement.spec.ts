377c321b6d66e9d9a86a67a0520dbd8a8e8cdcbc
Sandro Chinea <sandrochinea@gmail.com>
2025-03-05 09:43:22 +0000

    A

--- modules/test/playwright/tests/address-web/countriesManagement.spec.ts
+++ modules/test/playwright/tests/address-web/countriesManagement.spec.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {expect, mergeTests} from '@playwright/test';
+import {expect, mergeTests} from '@playswright/test';
 
 import {countriesManagementPageTest} from '../../fixtures/CountriesManagementPageTest';
 import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
