bf3fd832204ee67b1d3d33442984b7068c9985d8
Sandro Chinea <sandrochinea@gmail.com>
2025-05-20 14:26:40 +0100

    LPD-56038 Add General panel content

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/ContentEditorSidePanel.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/content_editor/components/ContentEditorSidePanel.tsx
@@ -7,10 +7,18 @@ import '../../../css/content_editor/ContentEditorSidePanel.scss';
 
 import {Button, VerticalBar} from '@clayui/core';
 import ClayIcon from '@clayui/icon';
+import ClayLabel from '@clayui/label';
+import {sub} from 'frontend-js-web';
 import React from 'react';
 
+type Props = {
+	id: string;
+	type: string;
+	version: string;
+};
+
 type Item = {
-	component: React.ComponentType;
+	component: React.ComponentType<Props>;
 	divider?: boolean;
 	icon: string;
 	title: string;
@@ -24,7 +32,7 @@ const items: Item[] = [
 	},
 ];
 
-export default function ContentEditorSidePanel() {
+export default function ContentEditorSidePanel(props: Props) {
 	return (
 		<VerticalBar className="content-editor__side-panel">
 			<VerticalBar.Content items={items}>
@@ -39,7 +47,7 @@ export default function ContentEditorSidePanel() {
 								</div>
 							</div>
 
-							<Component />
+							<Component {...props} />
 						</VerticalBar.Panel>
 					);
 				}}
@@ -58,6 +66,38 @@ export default function ContentEditorSidePanel() {
 	);
 }
 
-function GeneralPanel() {
-	return <></>;
+function GeneralPanel({id, type, version}: Props) {
+	const Row = ({
+		children,
+		label,
+	}: {
+		children: React.ReactNode;
+		label: string;
+	}) => (
+		<div className="border-bottom d-flex justify-content-between mb-2 pb-2">
+			<span className="font-weight-semi-bold text-secondary">
+				{label}
+			</span>
+
+			{children}
+		</div>
+	);
+
+	return (
+		<div className="p-3">
+			<Row label={Liferay.Language.get('type')}>
+				<span>{type}</span>
+			</Row>
+
+			<Row label={Liferay.Language.get('version')}>
+				<ClayLabel displayType="info">
+					{sub(Liferay.Language.get('version-x'), version)}
+				</ClayLabel>
+			</Row>
+
+			<Row label={Liferay.Language.get('id')}>
+				<span>{id}</span>
+			</Row>
+		</div>
+	);
 }
