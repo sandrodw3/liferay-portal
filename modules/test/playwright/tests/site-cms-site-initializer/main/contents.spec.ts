61da75715b8dd6b785fa90391ef6c20a734a049e
Sandro Chinea <sandrochinea@gmail.com>
2025-08-21 13:17:24 +0100

    LPD-62179 Add test

--- modules/test/playwright/tests/site-cms-site-initializer/main/contents.spec.ts
+++ modules/test/playwright/tests/site-cms-site-initializer/main/contents.spec.ts
@@ -7,12 +7,16 @@ import {expect, mergeTests} from '@playwright/test';
 
 import {dataApiHelpersTest} from '../../../fixtures/dataApiHelpersTest';
 import {featureFlagsTest} from '../../../fixtures/featureFlagsTest';
+import {fragmentsPagesTest} from '../../../fixtures/fragmentPagesTest';
 import {loginTest} from '../../../fixtures/loginTest';
 import {pageEditorPagesTest} from '../../../fixtures/pageEditorPagesTest';
+import {clickAndExpectToBeVisible} from '../../../utils/clickAndExpectToBeVisible';
 import {getRandomInt} from '../../../utils/getRandomInt';
 import getRandomString from '../../../utils/getRandomString';
+import {waitForAlert} from '../../../utils/waitForAlert';
 import {structureBuilderPagesTest} from '../structure-builder/fixtures/structureBuilderPagesTest';
 import {cmsPagesTest} from './fixtures/cmsPagesTest';
+import {PicklistBuilderPage} from './pages/PicklistBuilderPage';
 
 const test = mergeTests(
 	cmsPagesTest,
@@ -23,6 +27,7 @@ const test = mergeTests(
 		'LPS-179669': {enabled: true},
 	}),
 	loginTest(),
+	fragmentsPagesTest,
 	pageEditorPagesTest,
 	structureBuilderPagesTest
 );
@@ -84,5 +89,145 @@ test(
 		await contentsPage.goto();
 
 		await contentsPage.deleteContent(title);
+	}
+);
+
+test(
+	'Create a picklist from the structure builder by opening other tab',
+	{tag: '@LPD-62179'},
+	async ({
+		contentsPage,
+		context,
+		localizationSelectPage,
+		page,
+		structureBuilderPage,
+	}) => {
+
+		// Create new structure
+
+		const structureLabel = `StructureName${getRandomInt()}`;
+
+		await structureBuilderPage.createStructureFromData({
+			label: structureLabel,
+			page: structureBuilderPage,
+			structureIds,
+		});
+
+		// Add a Single Select field and Multiselect fields
+
+		await structureBuilderPage.addField('Single Select');
+		await structureBuilderPage.addField('Multiselect');
+
+		// Create new picklist from the button "New Picklist"
+
+		await structureBuilderPage.selectFields([{label: 'Single Select'}]);
+
+		const pagePromise = context.waitForEvent('page');
+
+		await page.getByText('New Picklist').click();
+
+		const newPage = await pagePromise;
+
+		// The picklist builder opens in a new tab
+
+		const picklistBuilderPage = new PicklistBuilderPage(newPage);
+
+		await expect(
+			newPage.getByRole('heading', {name: 'New Picklist'})
+		).toBeAttached();
+
+		const picklistName = getRandomString();
+
+		await newPage.getByLabel('Picklist Name').fill(picklistName);
+
+		// Add options
+
+		await picklistBuilderPage.addOption('Yellow');
+		await picklistBuilderPage.addOption('Blue');
+
+		// Save the picklist
+
+		await newPage.getByRole('button', {name: 'Save'}).click();
+
+		await waitForAlert(newPage, 'was saved successfully.');
+
+		// Select the picklist in both fields
+
+		const picklistPicker = page.getByLabel('Picklist');
+
+		await clickAndExpectToBeVisible({
+			autoClick: true,
+			target: page.getByRole('option', {name: picklistName}),
+			trigger: picklistPicker,
+		});
+
+		await structureBuilderPage.selectFields([{label: 'Multiselect'}]);
+
+		await clickAndExpectToBeVisible({
+			autoClick: true,
+			target: page.getByRole('option', {name: picklistName}),
+			trigger: picklistPicker,
+		});
+
+		// Publish the structure
+
+		await structureBuilderPage.publishStructure();
+
+		// Now create a content for this structure
+
+		await contentsPage.goto();
+
+		await contentsPage.createContent(structureLabel);
+
+		// Fill data in default language
+
+		const contentTitle = getRandomString();
+
+		await contentsPage.fillData([{label: 'Title', value: contentTitle}]);
+
+		await clickAndExpectToBeVisible({
+			autoClick: true,
+			target: page.getByRole('option', {name: 'Yellow'}),
+			trigger: page.locator('.select-from-list'),
+		});
+
+		await page.getByLabel('Blue').check();
+
+		// Switch to spanish and change values
+
+		await localizationSelectPage.switchLanguage('es-ES');
+
+		await clickAndExpectToBeVisible({
+			autoClick: true,
+			target: page.getByRole('option', {name: 'Blue'}),
+			trigger: page.locator('.select-from-list'),
+		});
+
+		await page.getByLabel('Yellow').check();
+
+		// Save content, edit it again and check values were persisted
+
+		await contentsPage.saveContent();
+
+		await contentsPage.editContent(contentTitle);
+
+		await expect(page.getByLabel('Blue')).toBeChecked();
+
+		await expect(page.getByPlaceholder('Choose an Option')).toHaveValue(
+			'Yellow'
+		);
+
+		await localizationSelectPage.switchLanguage('es-ES');
+
+		await expect(page.getByLabel('Blue')).toBeChecked();
+		await expect(page.getByLabel('Yellow')).toBeChecked();
+
+		await expect(page.getByLabel('Single Select')).toHaveValue('Blue');
+
+		// Delete picklist
+
+		const picklist = await picklistBuilderPage.getPicklist(picklistName);
+
+		await picklistBuilderPage.deletePicklist(picklist.id);
 	}
 );
