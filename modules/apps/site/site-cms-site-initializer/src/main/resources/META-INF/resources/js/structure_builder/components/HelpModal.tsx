123c397abe5b02de54f6eee29fc939dd2deaddb7
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-01-08 16:13:07 +0000

    WIP

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/components/HelpModal.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/components/HelpModal.tsx
@@ -4,6 +4,8 @@
  */
 
 import {ClayButtonWithIcon} from '@clayui/button';
+import ClayModal from '@clayui/modal';
+import {openModal} from 'frontend-js-components-web';
 import React from 'react';
 
 export function HelpModalTrigger() {
@@ -12,7 +14,25 @@ export function HelpModalTrigger() {
 			aria-label={Liferay.Language.get('open-help-modal')}
 			className="bg-dark m-5 position-fixed rounded-circle structure-builder__help-modal-trigger text-white"
 			displayType="unstyled"
+			onClick={() =>
+				openModal({
+					center: true,
+					contentComponent: HelpModalContent,
+				})
+			}
 			symbol="question-circle"
 		/>
+	);
+}
+
+function HelpModalContent() {
+	return (
+		<>
+			<ClayModal.Header>
+				{Liferay.Language.get('help-and-shortcuts')}
+			</ClayModal.Header>
+
+			<ClayModal.Body></ClayModal.Body>
+		</>
 	);
 }
