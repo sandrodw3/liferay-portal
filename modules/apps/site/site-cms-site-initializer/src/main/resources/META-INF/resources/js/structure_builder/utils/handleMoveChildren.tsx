4fdfc334083db92df589e776eb8c9971a645d40c
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-02-18 15:14:50 +0000

    LPD-79027 Use correct default value

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/utils/handleMoveChildren.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/utils/handleMoveChildren.tsx
@@ -88,6 +88,7 @@ export default async function handleMoveChildren({
 
 	if (hasNameConflict(movableItems, target)) {
 		const onNameConflict = await openOptionsModal({
+			defaultValue: 'rename',
 			options: [
 				{
 					label: Liferay.Language.get('do-not-move'),
