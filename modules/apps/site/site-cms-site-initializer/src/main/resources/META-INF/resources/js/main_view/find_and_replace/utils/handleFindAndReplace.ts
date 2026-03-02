26a693354ffd51fab97b33414f0a1b78c746a2de
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-03-02 10:14:46 +0000

    LPD-81627 Enable find and replace

---
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/find_and_replace/utils/handleFindAndReplace.ts
@@ -0,0 +1,63 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {readConfigFromURL} from '@liferay/frontend-data-set-web';
+import {openConfirmModal} from '@liferay/layout-js-components-web';
+
+import {ISearchAssetObjectEntry} from '../../../common/types/AssetType';
+import {openFindAndReplaceModal} from './openFindAndReplaceModal';
+
+export async function handleFindAndReplace({
+	availableLocales,
+	dataSetId,
+	fdsItems,
+}: {
+	availableLocales: Locale[];
+	dataSetId?: string;
+	fdsItems: ISearchAssetObjectEntry[];
+}) {
+	if (!dataSetId) {
+		return;
+	}
+
+	const search = getSearchFromURL(dataSetId);
+
+	if (!search) {
+		showWarning({
+			text: Liferay.Language.get(
+				'to-use-find-and-replace-you-need-to-perform-a-search-first'
+			),
+			title: Liferay.Language.get('search-required'),
+		});
+
+		return;
+	}
+
+	openFindAndReplaceModal({availableLocales, fdsItems, search});
+}
+
+function getSearchFromURL(dataSetId: string): string | null {
+	if (!dataSetId) {
+		return null;
+	}
+
+	const config = readConfigFromURL(dataSetId);
+
+	if (!config || !config.q) {
+		return null;
+	}
+
+	return config.q || null;
+}
+
+function showWarning({text, title}: {text: string; title: string}) {
+	openConfirmModal({
+		buttonLabel: Liferay.Language.get('ok'),
+		center: true,
+		status: 'warning',
+		text,
+		title,
+	});
+}
---
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/find_and_replace/utils/openFindAndReplaceModal.ts
@@ -0,0 +1,22 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {render} from '@liferay/frontend-js-react-web';
+
+import {ISearchAssetObjectEntry} from '../../../common/types/AssetType';
+import FindAndReplaceModal from '../components/FindAndReplaceModal';
+
+type Props = {
+	availableLocales: Locale[];
+	fdsItems: ISearchAssetObjectEntry[];
+	search: string;
+};
+
+export function openFindAndReplaceModal(props: Props) {
+
+	// @ts-ignore
+
+	return render(FindAndReplaceModal, props, document.createElement('div'));
+}
