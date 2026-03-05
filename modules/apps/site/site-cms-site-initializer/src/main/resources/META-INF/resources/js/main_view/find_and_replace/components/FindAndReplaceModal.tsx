6cf7828d513b78869f8622000447825dc8717002
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-03-05 14:18:35 +0000

    LPD-81627 Add main modal component

---
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main_view/find_and_replace/components/FindAndReplaceModal.tsx
@@ -0,0 +1,93 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import ClayModal, {useModal} from '@clayui/modal';
+import {Observer} from '@clayui/modal/src/types';
+import {Locale} from 'frontend-js-components-web';
+import React, {useContext, useState} from 'react';
+
+import {ISearchAssetObjectEntry} from '../../../common/types/AssetType';
+import {
+	FindAndReplaceContext,
+	FindAndReplaceContextProvider,
+	View,
+} from '../contexts/FindAndReplaceContext';
+import {Discard} from './Discard';
+import {Loading} from './Loading';
+import {NoMatches} from './NoMatches';
+import {Setup} from './Setup';
+import {Summary} from './Summary';
+
+type Props = {
+	availableLocales: Locale[];
+	fdsItems: ISearchAssetObjectEntry[];
+	search: string;
+};
+
+export default function Wrapper({availableLocales, fdsItems, search}: Props) {
+	const [visible, setVisible] = useState(true);
+
+	const {observer, onClose: closeModal} = useModal({
+		onClose: () => setVisible(false),
+	});
+
+	if (!visible) {
+		return;
+	}
+
+	return (
+		<FindAndReplaceContextProvider
+			availableLocales={availableLocales}
+			closeModal={closeModal}
+			fdsItems={fdsItems}
+			search={search}
+		>
+			<FindAndReplaceModal observer={observer} />
+		</FindAndReplaceContextProvider>
+	);
+}
+
+function FindAndReplaceModal({observer}: {observer: Observer}) {
+	const {view} = useContext(FindAndReplaceContext);
+
+	const size = getSize(view);
+	const status = getStatus(view);
+
+	return (
+		<ClayModal
+			center={size !== 'full-screen'}
+			disableAutoClose
+			observer={observer}
+			size={size}
+			status={status}
+		>
+			{view === 'loading' && <Loading />}
+
+			{view === 'no-matches' && <NoMatches />}
+
+			{view === 'setup' && <Setup />}
+
+			{view === 'summary' && <Summary />}
+
+			{view === 'discard' && <Discard />}
+		</ClayModal>
+	);
+}
+
+function getSize(view: View) {
+	if (view === 'summary') {
+		return 'full-screen';
+	}
+
+	return undefined;
+}
+
+function getStatus(view: View) {
+	if (view === 'no-matches' || view === 'discard') {
+		return 'danger';
+	}
+
+	return undefined;
+}
