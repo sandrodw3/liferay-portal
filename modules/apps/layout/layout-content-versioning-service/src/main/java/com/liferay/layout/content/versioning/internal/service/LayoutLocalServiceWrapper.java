02bd6d38825d41f3a450e1975f1d3bddd84d2ce1
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-05-12 09:33:31 +0200

    LPD-90201 Snapshot a LayoutContentVersion when copyLayoutContent fires from publish

---
+++ modules/apps/layout/layout-content-versioning-service/src/main/java/com/liferay/layout/content/versioning/internal/service/LayoutLocalServiceWrapper.java
@@ -0,0 +1,102 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.layout.content.versioning.internal.service;
+
+import com.liferay.layout.content.versioning.provider.LayoutContentVersionDataProvider;
+import com.liferay.layout.content.versioning.service.LayoutContentVersionLocalService;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
+import com.liferay.layout.utility.page.model.LayoutUtilityPageEntry;
+import com.liferay.layout.utility.page.service.LayoutUtilityPageEntryLocalService;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.ServiceWrapper;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(service = ServiceWrapper.class)
+public class LayoutLocalServiceWrapper
+	extends com.liferay.portal.kernel.service.LayoutLocalServiceWrapper {
+
+	@Override
+	public Layout copyLayoutContent(Layout sourceLayout, Layout targetLayout)
+		throws Exception {
+
+		Layout layout = super.copyLayoutContent(sourceLayout, targetLayout);
+
+		_addLayoutContentVersion(sourceLayout, targetLayout);
+
+		return layout;
+	}
+
+	private void _addLayoutContentVersion(
+		Layout sourceLayout, Layout targetLayout) {
+
+		try {
+			long targetPlid = targetLayout.getPlid();
+
+			if ((sourceLayout.getClassPK() != targetPlid) ||
+				!targetLayout.isTypeContent()) {
+
+				return;
+			}
+
+			LayoutPageTemplateEntry layoutPageTemplateEntry =
+				_layoutPageTemplateEntryLocalService.
+					fetchLayoutPageTemplateEntryByPlid(targetPlid);
+
+			if (layoutPageTemplateEntry != null) {
+				return;
+			}
+
+			LayoutUtilityPageEntry layoutUtilityPageEntry =
+				_layoutUtilityPageEntryLocalService.
+					fetchLayoutUtilityPageEntryByPlid(targetPlid);
+
+			if (layoutUtilityPageEntry != null) {
+				return;
+			}
+
+			_layoutContentVersionLocalService.addLayoutContentVersion(
+				null, sourceLayout.getUserId(), sourceLayout.getPlid(),
+				targetLayout.getName(LocaleUtil.getSiteDefault()),
+				_layoutContentVersionDataProvider.getLayoutContentVersionData(
+					sourceLayout),
+				WorkflowConstants.STATUS_APPROVED, true);
+		}
+		catch (Exception exception) {
+			_log.error(
+				"Unable to add LayoutContentVersion on publish for plid " +
+					sourceLayout.getPlid(),
+				exception);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LayoutLocalServiceWrapper.class);
+
+	@Reference
+	private LayoutContentVersionDataProvider _layoutContentVersionDataProvider;
+
+	@Reference
+	private LayoutContentVersionLocalService _layoutContentVersionLocalService;
+
+	@Reference
+	private LayoutPageTemplateEntryLocalService
+		_layoutPageTemplateEntryLocalService;
+
+	@Reference
+	private LayoutUtilityPageEntryLocalService
+		_layoutUtilityPageEntryLocalService;
+
+}
