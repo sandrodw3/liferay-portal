b3e8342c6ffd7d28f5d76663f7c2fc4541cac611
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-05-31 22:25:23 +0200

    LPD-88081 Rename testUpdateStyleBookEntryERCWithScopePersistsBothColumns to testUpdateStyleBookEntry

--- modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/service/test/LayoutLocalServiceTest.java
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/service/test/LayoutLocalServiceTest.java
@@ -1109,8 +1109,7 @@ public class LayoutLocalServiceTest {
 
 	@Test
 	@TestInfo("LPD-88081")
-	public void testUpdateStyleBookEntryERCWithScopePersistsBothColumns()
-		throws Exception {
+	public void testUpdateStyleBookEntry() throws Exception {
 
 		Layout layout = LayoutTestUtil.addTypePortletLayout(_group);
 
