4efb25159e9038aa6ef119f9ab9f97004ab2782c
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2026-06-01 19:47:21 +0200

    LPD-92981 Add Toolbar

--- modules/apps/layout/layout-content-versioning-web/src/main/resources/META-INF/resources/js/PageVersionHistory.tsx
+++ modules/apps/layout/layout-content-versioning-web/src/main/resources/META-INF/resources/js/PageVersionHistory.tsx
@@ -3,8 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {ClayButtonWithIcon} from '@clayui/button';
 import {SidePanel} from '@clayui/core';
-import React, {useRef} from 'react';
+import ClayToolbar from '@clayui/toolbar';
+import {sub} from 'frontend-js-web';
+import React, {useRef, useState} from 'react';
 
 import './PageVersionHistory.scss';
 
@@ -15,12 +18,36 @@ interface Props {
 export default function PageVersionHistory({
 	pageSpecificationVersionsURL,
 }: Props) {
+	const [isOpen, setIsOpen] = useState(true);
+
 	const wrapperRef = useRef<HTMLElement | null>(
 		document.getElementById('wrapper')
 	);
 
 	return (
 		<>
+			<ClayToolbar className="bg-white page-version-history__toolbar">
+				{!isOpen ? (
+					<ClayToolbar.Nav>
+						<ClayToolbar.Item>
+							<ClayButtonWithIcon
+								displayType="secondary"
+								onClick={() => setIsOpen(true)}
+								size="sm"
+								symbol="angle-double-right"
+								title={sub(
+									Liferay.Language.get('open-x'),
+									sub(
+										Liferay.Language.get('x-panel'),
+										Liferay.Language.get('version-history')
+									)
+								)}
+							/>
+						</ClayToolbar.Item>
+					</ClayToolbar.Nav>
+				) : null}
+			</ClayToolbar>
+
 			<SidePanel
 				className="page-version-history__side-panel"
 				containerRef={wrapperRef}
