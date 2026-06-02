06d1d4680cbd14009cb7155e94e81e4e3920ab60
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2026-06-02 10:43:56 +0200

    LPD-90025 Add tests for deriving the LayoutContentVersion name from the layout

--- modules/apps/layout/layout-content-versioning-test/src/testIntegration/java/com/liferay/layout/content/versioning/service/test/LayoutContentVersionLocalServiceTest.java
+++ modules/apps/layout/layout-content-versioning-test/src/testIntegration/java/com/liferay/layout/content/versioning/service/test/LayoutContentVersionLocalServiceTest.java
@@ -16,6 +16,7 @@ import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.FeatureFlag;
 import com.liferay.portal.test.rule.Inject;
@@ -83,6 +84,7 @@ public class LayoutContentVersionLocalServiceTest {
 			approvedLayoutContentVersion.getStatus());
 
 		_testAddLayoutContentVersionWithNullExternalReferenceCode();
+		_testAddLayoutContentVersionWithNullName();
 		_testAddLayoutContentVersionWithSkipIfUnchanged();
 	}
 
@@ -120,6 +122,18 @@ public class LayoutContentVersionLocalServiceTest {
 			_draftLayout.getExternalReferenceCode() + "_v_" +
 				layoutContentVersion.getVersion(),
 			layoutContentVersion.getExternalReferenceCode());
+	}
+
+	private void _testAddLayoutContentVersionWithNullName() throws Exception {
+		LayoutContentVersion layoutContentVersion =
+			_layoutContentVersionLocalService.addLayoutContentVersion(
+				RandomTestUtil.randomString(), TestPropsValues.getUserId(),
+				_draftLayout.getPlid(), null, RandomTestUtil.randomString(),
+				WorkflowConstants.STATUS_DRAFT, false);
+
+		Assert.assertEquals(
+			_draftLayout.getName(LocaleUtil.getSiteDefault()),
+			layoutContentVersion.getName(LocaleUtil.getSiteDefault()));
 	}
 
 	private void _testAddLayoutContentVersionWithSkipIfUnchanged()
